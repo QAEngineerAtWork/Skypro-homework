8450e93c36870a42de7e338f09318bb79217e90e
QAEngineerAtWork <galkovamb@gmail.com>
2023-12-22 00:23:27 +0300

    Удалили тестовые файлы, прикрепили файлы с домашкой

---
+++ lesson_1_task_4.py
@@ -0,0 +1,3 @@
+first_name = input("Ваше имя:")
+last_name = input("Вашa фамилия: ")
+print("Вас зовут: " +last_name +" " +first_name)
---
+++ lesson_1_task_6.py
@@ -0,0 +1,11 @@
+def print_greeting():
+    print("Привет, мир!")
+
+print_greeting()
+
+# def greeting():
+#     x = "Привет, мир!"
+
+#     return x
+
+# print(greeting) 
